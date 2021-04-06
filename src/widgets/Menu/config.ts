4daab8b9e57b403b0a7f080ca982a17e8b099a3c
MarsSwapFinance <marsswap@tutanota.com>
2021-04-07 00:49:29 +0200

    Add uikit

---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,101 @@
+export const links = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  {
+    label: 'Exchange',
+    href: 'https://exchange.marsswap.finance/swap',
+    icon: "ExchangeIcon"
+  },
+  {
+    label: 'Liquidity',
+    href: 'https://exchange.marsswap.finance/pool',
+    icon: 'LiquidityIcon'
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: '/farms',
+  },
+  {
+    label: 'Pools',
+    icon: 'PoolIcon',
+    href: '/pools',
+  },
+  {
+    label: 'Referrals',
+    icon: 'GroupsIcon',
+    href: '/referrals',
+  },
+  {
+    label: 'Roadmap',
+    icon: 'RoadmapIcon',
+    href: 'https://docs.marsswap.finance/roadmap',
+  },
+  // {
+  //   label: 'Info',
+  //   icon: 'InfoIcon',
+  //   items: [
+  //     {
+  //       label: 'PancakeSwap',
+  //       href: `https://pancakeswap.info/token/${frontendConfig.token['56']}`,
+  //     },
+  //   ],
+  // },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: 'Github',
+        href: 'https://github.com/MarsSwapFinance',
+      },
+      {
+        label: 'Docs',
+        href: 'https://docs.marsswap.finance/',
+      },
+      {
+        label: 'Blog',
+        href: 'https://medium.com/marsswap',
+      },
+      {
+        label: 'White Paper',
+        href: 'https://marsswap.finance/whitepaper.pdf',
+      },
+    ],
+  },
+]
+
+export const socials = [
+  {
+    label: "Telegram",
+    icon: "TelegramIcon",
+    items: [
+      {
+        label: "English",
+        href: "https://t.me/MarsSwap",
+      },
+      {
+        label: "Announcements",
+        href: "https://t.me/MarsSwap_News",
+      },
+    ],
+  },
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "https://twitter.com/marsswap",
+  },
+  {
+    label: "Reddit",
+    icon: "RedditIcon",
+    href: "https://www.reddit.com/r/", // TODO
+  },
+];
+
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
